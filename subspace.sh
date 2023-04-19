d657e5ac8618d2a9faec6419b4fe5fd99697e2eb
Alexander Shvets <76999674+soandwhat@users.noreply.github.com>
2023-04-19 20:33:42 +0300

    Create subspace.sh

---
+++ subspace.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+
+sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+cd $HOME
+wget -O subspace-cli https://github.com/subspace/subspace-cli/releases/download/v0.3.3-alpha/subspace-cli-ubuntu-aarch64-v0.3.3-alpha
+sudo chmod +x subspace-cli
+sudo mv subspace-cli /usr/local/bin/
+sudo rm -rf $HOME/.config/subspace-cli
+/usr/local/bin/subspace-cli init
+#systemctl stop subspaced subspaced-farmer &>/dev/null
+#rm -rf ~/.local/share/subspace*
+
+#source ~/.bash_profile
+sleep 1
+
+echo "[Unit]
+Description=Subspace Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/subspace-cli farm --verbose
+Restart=on-failure
+LimitNOFILE=1024000
+
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced.service
+
+mv $HOME/subspaced.service /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced
+sudo systemctl restart subspaced
+
+echo "==================================================="
+echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 5
+if [[ `service subspaced status | grep active` =~ "running" ]]; then
+  echo -e "Your Subspace node \e[32minstalled and works\e[39m!"
+  echo -e "You can check node status by the command \e[7mservice subspaced status\e[0m"
+  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+else
+  echo -e "Your Subspace node \e[31mwas not installed correctly\e[39m, please reinstall."
+fi
